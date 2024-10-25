b5786b2f383bf3716677bea308198bb056770eaf
ninitsikhelashvili <nino.tsikhelashvili10@gmail.com>
2024-10-25 18:19:06 +0400

    first commit

---
+++ group53/davaleba.py
@@ -0,0 +1,8 @@
+print("nino tsikhelashvili")
+
+
+name = "nino"
+surname = "tsikhelashvili"
+age = 14
+
+print(name + surname + str(age))
---
+++ group53/davaleba2.1.py
@@ -0,0 +1,6 @@
+name="nini"
+surname="tsikhelashvili"
+age=14
+favcolor="purple"
+
+print(name , surname , age , favcolor)
