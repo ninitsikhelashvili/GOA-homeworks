b8daf8d5ccd22ad540d389ee414aa88745bd1e70
ninitsikhelashvili <nino.tsikhelashvili10@gmail.com>
2024-11-01 15:48:21 +0400

    third commit

---
+++ group53/day2/classwork/saklaso.py
@@ -0,0 +1,30 @@
+name = "nini"
+surname = "tsikhelashvili"
+str = "shoe"
+str2 = "book"
+
+num1 = 10
+num2 = 170
+num3 = 15
+num4 = 20
+
+float1 = 3.5
+float2 = 5.7
+float3 = 9.4
+float4 = 5.2
+
+print(name , surname , str , str2 , num1 , num2 , num3 , num4 , float1 , float2 , float3 , float4)
+
+print(num1 + num2 + num3 + num4)
+print(num1 * num2 * num3 * num4)
+print(num1//num2//num3//num4)
+print(num1/num2/num3/num4)
+print(num1 - num2 - num3 - num4)
+
+print(float1 + float2 + float3 + float4)
+print(float1 - float2 - float3 - float4)
+print(float1 * float2 * float3 * float4)
+print(float1//float2//float3//float4)
+print(float1/float2/float3/float4)
+
+
---
+++ group53/day2/homework/davaleba3.py
@@ -0,0 +1,36 @@
+num1 = 5
+num2 = 15
+num3 = 30
+
+print(num1 + num2)
+print(num1 + num3)
+print(num2 + num3)
+
+print(num1 - num2)
+print(num1 - num3)
+print(num3 - num2)
+
+print(num1 * num2)
+print(num2 * num3)
+print(num3 * num1)
+
+print(num1//num2)
+print(num3//num2)
+print(num2//num1)
+
+print(num1**num2)
+print(num2**num1)
+print(num3**num2)
+
+print(num1 % num2)
+print(num2 % num1)
+print(num3 % num1)
+
+
+member1 = 45
+member2 = 53
+member3 = 26
+member4 = 22
+member5 = 14
+
+print(member1 + member2 + member3 + member4 + member5)
