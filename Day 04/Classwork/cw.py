87c8d067c6c3b4aa912acba10430749ca3bf3683
ninitsikhelashvili <nino.tsikhelashvili10@gmail.com>
2024-11-13 19:03:32 +0400

    first commit

---
+++ Day 04/Classwork/cw.py
@@ -0,0 +1,48 @@
+print(5+5)
+print(10+5)
+print(20+12)
+print(17+6)
+
+print(5-5)
+print(10-5)
+print(20-12)
+print(17-6)
+
+print(5*5)
+print(10*5)
+print(20*12)
+print(17*6)
+
+print(5/5)
+print(10/5)
+print(20/12)
+print(17/6)
+
+
+num1=45
+num2=53
+print(num1+20)
+print(num2+20)
+
+
+
+
+
+
+
+
+
+
+
+name1= "Zuka"     
+user_name = "Ana" 
+for2= 10                  
+class3 = "Math"             
+print("userAge") 
+age = 25
+print(age + int("years"))        
+Name = "Zuka"
+print("name")                  
+full_name = "Zuka K"         
+def3 = "Function"           
+userAge2 = 20
