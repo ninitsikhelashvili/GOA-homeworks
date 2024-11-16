f19f916ad1a78c16632fff030d7ec45508e2e98e
ninitsikhelashvili <nino.tsikhelashvili10@gmail.com>
2024-11-16 11:12:43 +0400

    second commit

---
+++ Day 06/Homework/HW1.py
@@ -0,0 +1,16 @@
+num1 = 5 + 8
+num2 = 19 - 17
+print(num1)
+print(num2)
+
+num3 = 15
+num4 = 60
+print(num3 * num4)
+print(num3 / num4)
+
+num5 = 3
+num5 = 4
+num5 = 5
+num5 = 6
+num5 = 7
+print(num5)
---
+++ Day 06/Homework/HW2.py
@@ -0,0 +1,20 @@
+from turtle import *
+
+width(6)
+color("green")
+begin_fill()
+forward(100)
+left(120)
+forward(100)
+left(120)
+forward(100)
+right(60)
+forward(150)
+right(120)
+forward(150)
+right(120)
+forward(150)
+end_fill()
+
+
+exitonclick()
