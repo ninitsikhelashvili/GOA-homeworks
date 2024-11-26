750c9cfcad7beae21dd122fa87482e4f0a60bb9f
ninitsikhelashvili <nino.tsikhelashvili10@gmail.com>
2024-11-26 15:35:00 +0400

    third commit

---
+++ Day 08/Classwork/lesson.py
@@ -0,0 +1,10 @@
+name = input("Please ennter your name: ")
+print(name)
+
+name1 = input("Please enter your surname: ")
+name2 = input("Please enter you birth place: ")
+
+print(name1 + name2)
+
+#input = informaciis shetana
+#output = shedegis migheba
---
+++ Day 08/Homework/homework.py
@@ -0,0 +1,12 @@
+# input = ინფორმაციის კომპიუტერში შეტანა
+# output = შედეგის მიღება 
+
+name = input("Pease enter your name: ")
+print(name + "," "wellcome")
+
+name2 = input("Please enter your surname: ")
+name3 = input("Please enter your age: ")
+print(name2)
+print(name3)
+print(name + name2 + name3)
+
