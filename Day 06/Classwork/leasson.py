114736caee9bb429b20dce1016fd73343718d67d
ninitsikhelashvili <nino.tsikhelashvili10@gmail.com>
2024-11-29 14:34:18 +0400

    fourth commit

---
+++ Day 06/Classwork/leasson.py
@@ -0,0 +1,30 @@
+num1 = 5
+num2 = 10
+num3 = 30
+num4 = 60
+
+print(num1 + num2)
+print(num2 + num3)
+print(num3 + num4)
+print(num1 + num4)
+
+print(num2 - num1)
+print(num3 - num2)
+print(num4 - num3)
+print(num4 - num1)
+
+print(num1 * num2)
+print(num2 * num3)
+print(num3 * num4)
+print(num4 * num1)
+
+print(num2 / num1)
+print(num3 / num2)
+print(num4 / num3)
+print(num3 / num1)
+
+member1 = 45
+member2 = 53
+
+print(member1 + 20)
+print(member2 + 20)
